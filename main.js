45207029b9b16052cb3f319a23c794b709b09445
Ethan Weikel <steath1999@gmail.com>
2018-11-14 10:30:42 -0800

    Added visual and description
    still figuring out how to get all the station counts to list out

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ $(updateView)
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
 function updateView(){
-    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/columbia_simpson" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/drake_park" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/g5" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/galveston" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/grc" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/old_mill" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/osu_cascades" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/ten_barrel" , updateRideCount)
 }
 //DOM
 function updateRideCount(data){
