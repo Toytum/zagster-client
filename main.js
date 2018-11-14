ccccd54cda2a368d92864aa576b6481ca7abe5a8
Ethan Weikel <steath1999@gmail.com>
2018-11-14 14:51:31 -0800

    Adding all data

--- main.js
+++ main.js
@@ -2,19 +2,24 @@ $(updateView)
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
 function updateView(){
-    $.getJSON(BASE_URL + "/rides/count/columbia_simpson" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/drake_park" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/g5" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/galveston" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/grc" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/old_mill" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/osu_cascades" , updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/ten_barrel" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/columbia_simpson" , printData)
+    $.getJSON(BASE_URL + "/rides/count/drake_park" , printData)
+    $.getJSON(BASE_URL + "/rides/count/g5" , printData)
+    $.getJSON(BASE_URL + "/rides/count/galveston" , printData)
+    $.getJSON(BASE_URL + "/rides/count/grc" , printData)
+    $.getJSON(BASE_URL + "/rides/count/old_mill" , printData)
+    $.getJSON(BASE_URL + "/rides/count/osu_cascades" , printData)
+    $.getJSON(BASE_URL + "/rides/count/ten_barrel" , printData)
 }
 //DOM
 function updateRideCount(data){
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
 }
+
+function printData(data){
+    console.log(data)
+}
+
 
 
