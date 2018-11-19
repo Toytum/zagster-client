3dec2177f002aa8b343710e652e89b1719adf0c8
Ethan Weikel <steath1999@gmail.com>
2018-11-19 15:26:13 -0800

    Cleaning up

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 $(updateView)
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
+var colSimpCount;
+
 function updateView(){
+    createGraph();
     $.getJSON(BASE_URL + "/rides/count/columbia_simpson" , printData)
     $.getJSON(BASE_URL + "/rides/count/drake_park" , printData)
     $.getJSON(BASE_URL + "/rides/count/g5" , printData)
@@ -10,53 +13,56 @@ function updateView(){
     $.getJSON(BASE_URL + "/rides/count/old_mill" , printData)
     $.getJSON(BASE_URL + "/rides/count/osu_cascades" , printData)
     $.getJSON(BASE_URL + "/rides/count/ten_barrel" , printData)
-
-var ctx = document.getElementById("polarChart");
-var polarChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
 }
-//DOM
+
 function updateRideCount(data){
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
 }
 
+// function updateColSimpCount(data){
+   // colSimpCount = // ...
+
 function printData(data){
     console.log(data)
 }
 
+function createGraph() {
+    var ctx = document.getElementById("polarChart");
+    var polarChart = new Chart(ctx, {
+        type: 'polarArea',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
