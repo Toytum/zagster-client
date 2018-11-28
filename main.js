90640c12dc2bb6504dec8c013706148d2e9297bb
Ethan Weikel <steath1999@gmail.com>
2018-11-28 14:02:44 -0800

    AGAHGAAHGGHA
    GRAPH ISN'T WORKING

--- main.js
+++ main.js
@@ -1,34 +1,59 @@
 $(updateView)
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-var colSimpCount;
+var stationData = [];
+var stationColSimpson;
+var stationDrakePark;
+var stationG5;
+var stationGalveston;
+var stationGrc;
+var stationOldMill;
+var stationOsuCascades;
+var stationTenBarrel;
 
 function updateView(){
     createGraph();
-    $.getJSON(BASE_URL + "/rides/count/columbia_simpson" , updateColSimpCount)
-    $.getJSON(BASE_URL + "/rides/count/drake_park" , printData)
-    $.getJSON(BASE_URL + "/rides/count/g5" , printData)
-    $.getJSON(BASE_URL + "/rides/count/galveston" , printData)
-    $.getJSON(BASE_URL + "/rides/count/grc" , printData)
-    $.getJSON(BASE_URL + "/rides/count/old_mill" , printData)
-    $.getJSON(BASE_URL + "/rides/count/osu_cascades" , printData)
-    $.getJSON(BASE_URL + "/rides/count/ten_barrel" , printData)
+  $.when(
+    $.getJSON(BASE_URL + "/rides/count/columbia_simpson" , updateColSimpson),
+    $.getJSON(BASE_URL + "/rides/count/drake_park" , updateDrakePark),
+    $.getJSON(BASE_URL + "/rides/count/g5" , updateG5),
+    $.getJSON(BASE_URL + "/rides/count/galveston" , updateGalveston),
+    $.getJSON(BASE_URL + "/rides/count/grc" , updateGrc),
+    $.getJSON(BASE_URL + "/rides/count/old_mill" , updateOldMill),
+    $.getJSON(BASE_URL + "/rides/count/osu_cascades" , updateOsuCascades),
+    $.getJSON(BASE_URL + "/rides/count/ten_barrel" , updateTenBarrel),
+  ).then(
+      makeStationGraph
+  );
+    
 }
 
-function updateRideCount(data){
-    numberOfRides = data.count
-    $("h2#rideCount").html(numberOfRides)
+function updateColSimpson(data){
+    stationColSimpson = data.count;
+}
+function updateDrakePark(data){
+    stationDrakePark = data.count;
+}
+function updateG5(data){
+    stationG5 = data.count;
+}
+function updateGalveston(data){
+    stationGalveston = data.count
+}
+function updateGrc(data){
+    stationGrc = data.count;
+}
+function updateOldMill(data){
+    stationOldMill = data.count;
+}
+function updateOsuCascades(data){
+    stationOsuCascades = data.count;
+}
+function updateTenBarrel(data){
+    stationTenBarrel = data.count;
 }
 
-function updateColSimpCount(data){
-
-}
-
-function printData(data){
-    console.log(data)
-}   
-
-function createGraph() {
+function makeStationGraph() {
     var ctx = document.getElementById("polarChart");
     var polarChart = new Chart(ctx, {
         type: 'polarArea',
@@ -36,7 +61,7 @@ function createGraph() {
             labels: ["Comlumbia Simpson", "Blue", "Yellow", "Green", "Purple", "Orange"],
             datasets: [{
                 label: '# of Votes',
-                data: [colSimpCount, 9, 3, 5, 2, 3],
+                data: [stationColSimpson, 9, 3, 5, 2, 3],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
